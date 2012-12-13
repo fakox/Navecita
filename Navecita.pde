60cf005c012359613b469d85a2cb2d156c2ad027
Francisco Cantillana Osorio <francisco.cantillanao@gmail.com>
2012-12-12 22:48:27 -0300

    Crear las Balas ( Bolas)+ Actualización.
    Creo un .pde en base al modelo de la clase Nave para poder dar
    estructura a las bolas. Se actualiza lo que se lleva en cuanto a la
    estructura de la clase Nave

--- Navecita.pde
+++ Navecita.pde
@@ -1,11 +1,16 @@
 Nave nave;
 
 void setup(){
+	size(512, 512);
 	nave = new Nave();
 	nave.setup();
 }
 
 void draw(){
-		nave.update();
-		nave.draw();
+	nave.update();
+
+	background(255);
+	nave.draw();
 }
+
+
---
+++ build-tmp/source/Navecita.java
@@ -0,0 +1,13 @@
+import processing.core.*; 
+import processing.data.*; 
+import processing.event.*; 
+import processing.opengl.*; 
+
+import java.util.HashMap; 
+import java.util.ArrayList; 
+import java.io.BufferedReader; 
+import java.io.PrintWriter; 
+import java.io.InputStream; 
+import java.io.OutputStream; 
+import java.io.IOException; 
+
